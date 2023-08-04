e2e581ed29114aba37dea535e5dd1fff9896cd63
apetazzi <alberto.petazzi@sm-optics.com>
2023-08-04 11:08:03 +0200

    Update done time ago...

---
+++ src/main/java/example/designPattern/builder/erDiagram3/ModelBuilder.java
@@ -0,0 +1,20 @@
+package example.designPattern.builder.erDiagram3;
+
+public abstract class ModelBuilder {
+
+	public ModelBuilder() {
+	}
+
+	void addEntity(String name) {
+	}
+
+	void addRelationship(String from, String to, String name) {
+	}
+
+	void addCardMin(String entity, String relation, String value) {
+	}
+
+	Object getModel() {
+		return null;
+	}
+}
